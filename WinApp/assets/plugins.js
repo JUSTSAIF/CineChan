c172a7ab90c6b5866c9093986692e320a166bdb5
JUSTSAIF <saifalcigeary@gmail.com>
2023-07-13 17:11:15 +0300

    add pause/resume Bu & ep number & title

--- WinApp/assets/plugins.js
+++ WinApp/assets/plugins.js
@@ -1,7 +1,13 @@
 const { ipcRenderer } = require('electron');
 
 const downloadBu = document.getElementById('download');
+// const downloadTranBu = document.getElementById('downloadTran');
 const cancelBu = document.getElementById('cancel');
+const pause_resumeBu = document.getElementById('pause_resumeDownload');
+const progressBar = document.getElementById('progress-bar-inner')
+const downloadInfo = document.getElementById('downloadInfo')
+
+let isPaused = false;
 let VidId = null;
 
 const createOption = (text, value) => {
@@ -27,11 +33,12 @@ ipcRenderer.on('vidownloader-options', (event, data) => {
     if (!vidEle) return;
     const tranEle = document.getElementById('translation');
     if (!tranEle) return;
-    
-    const { vid, tran, id, icon, title } = data
+
+    const { vid, tran, id, icon, title, ep } = data
     VidId = id;
     document.getElementById("mv-img").src = icon;
-    document.getElementById("title").textContent = title;
+    epTxt = ep != null && ep != "0" && ep != 0 && ep != undefined ? "  ,EP " + ep : ""
+    document.getElementById("title").textContent = title +epTxt;
 
     vid.forEach(vid => {
         const option = createOption(`${vid.resolution} - ${vid.container}`, `${vid.videoUrl}`);
@@ -53,6 +60,7 @@ ipcRenderer.on('vidownloader-options', (event, data) => {
 
 downloadBu.addEventListener('click', () => {
     cancelBu.disabled = false;
+    pause_resumeBu.disabled = false;
     downloadBu.disabled = true;
     const SelectedVid = document.getElementById("resolution")
     const SelectedTran = document.getElementById("translation")
@@ -61,9 +69,20 @@ downloadBu.addEventListener('click', () => {
         tran: SelectedTran?.value,
         vidName: SelectedVid?.options[SelectedVid?.selectedIndex]?.textContent,
         tranName: SelectedTran?.options[SelectedTran?.selectedIndex]?.textContent,
+        title: document.getElementById("title").textContent,
         id: VidId
     });
 });
+
+// downloadTranBu.addEventListener('click', () => {
+//     const SelectedTran = document.getElementById("translation")
+//     ipcRenderer.send('download-tran', {
+//         tran: SelectedTran?.value,
+//         tranName: SelectedTran?.options[SelectedTran?.selectedIndex]?.textContent,
+//         title: document.getElementById("title").textContent,
+//         id: VidId
+//     });
+// });
 
 cancelBu.addEventListener('click', () => {
     ipcRenderer.send('cancel-download', VidId);
@@ -77,11 +96,11 @@ cancelBu.addEventListener('click', () => {
 
 ipcRenderer.on('downloadProgress', (event, percentage) => {
     if (cancelBu.disabled == false) {
-        var progressBar = document.getElementById('progress-bar-inner')
         progressBar.textContent = '';
         if (percentage >= 100) {
             progressBar.textContent = 'Done';
             cancelBu.disabled = true;
+            pause_resumeBu.disabled = true;
             downloadBu.disabled = false;
         }
         progressBar.style.width = `${percentage}%`;
@@ -90,12 +109,37 @@ ipcRenderer.on('downloadProgress', (event, percentage) => {
 });
 
 ipcRenderer.on('downloadSize', (event, downloadSize) => {
-    var i = document.getElementById('size');
-    i.textContent = `${Sizeformater(downloadSize)}`;
+    var i = downloadInfo.querySelector('#size');
+    i.textContent = `File Size : ${Sizeformater(downloadSize)}`;
     i.style.display = "block";
 });
 
-ipcRenderer.on('dwn-path-exit', () => {
+ipcRenderer.on('downloadSpeed', (event, downloadSpeed) => {
+    downloadInfo.querySelector("#speed").textContent = `SPEED : ${Sizeformater(downloadSpeed)}`
+});
+
+ipcRenderer.on('downloadErr', (event) => {
+    downloadInfo.querySelector("#err").textContent = `ERR : TRUE`
+});
+
+ipcRenderer.on('downloadState', (event, state) => {
+    downloadInfo.querySelector("#state").textContent = `STATE : ${state}`
+});
+
+ipcRenderer.on('dwn-path-exit', (event) => {
     cancelBu.disabled = true;
+    pause_resumeBu.disabled = true;
     downloadBu.disabled = false;
+});
+
+
+pause_resumeBu.addEventListener('click', () => {
+    isPaused = !isPaused;
+    if (isPaused) {
+        pause_resumeBu.querySelector('img').src = './assets/icons/resume.png'
+        ipcRenderer.send('pause-download', VidId);
+    } else {
+        pause_resumeBu.querySelector('img').src = './assets/icons/pause.png'
+        ipcRenderer.send('resume-download', VidId);
+    }
 });
